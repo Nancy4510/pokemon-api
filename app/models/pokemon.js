f18422adc214e297978537430676b86843bffbcd
Nancy4510 <nancy4510@gmail.com>
2019-12-02 12:26:00 -0500

    Created a routes and models file
    I created a Pokemon models file and a routes file as well.

---
+++ app/models/pokemon.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const entrySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    required: true
+  },
+  attack: {
+    type: String,
+    required: true
+  },
+  weakness: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  timestamps: true,
+  // in case we need to use virtuals, this allows for it
+  toObject: { virtuals: true },
+  toJSON: { virtuals: true }
+})
+
+module.exports = mongoose.model('Pokemon', entrySchema)
